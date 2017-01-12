b7905bb097bde2388c09260b75d9cd5db73dc406
HARISMEHMOOD <haris.mehmood@outlook.com>
2017-01-12 16:27:21 +0400

    u

--- config/module.config.php
+++ config/module.config.php
@@ -11,4 +11,56 @@
 namespace AlphaPage;
 
 return array(
+    'controllers' => array(
+        'factories' => array(
+            'AlphaPage\Controller\Page' => 'AlphaPage\Controller\PageControllerFactory',
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'AlphaPage\Service\Page' => 'AlphaPage\Service\PageServiceFactory',
+        ),
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                )
+            )
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'alpha-page' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route' => '[/:name]',
+                    'defaults' => array(
+                        'controller' => 'AlphaPage\Controller\Page',
+                        'action' => 'view',
+                    ),
+                ),
+            ),
+            'crud-page' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route' => '/page[/:action][/:name]',
+                    'defaults' => array(
+                        'controller' => 'AlphaPage\Controller\Page',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
 );
