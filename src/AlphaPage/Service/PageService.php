f31ad11fd22b054e0ae8037def2c62497b88feca
HARISMEHMOOD <haris.mehmood@outlook.com>
2017-02-01 21:14:11 +0400

    u

--- src/AlphaPage/Service/PageService.php
+++ src/AlphaPage/Service/PageService.php
@@ -4,6 +4,8 @@ namespace AlphaPage\Service;
 
 use Doctrine\ORM\EntityManager;
 use AlphaUserBase\Service\UserService;
+use Alpha\Entity\AlphaRoute;
+use AlphaPage\Entity\Page;
 
 /**
  * @author Haris Mehmood <haris.mehmood@outlook.com>
@@ -37,6 +39,44 @@ class PageService {
         $page->setEditor($user);
         $this->entityManager->flush();
         return $page;
+    }
+
+    public function createPage($routeName, $code, $user) {
+        $now = date_create(date('Y-m-d H:i:s'));
+
+        $page = new Page();
+        $page->setName($routeName);
+        $page->setEditor($user);
+        $page->setContent($code);
+        $page->setLastModified($now);
+        $page->setLayout(NULL);
+        $this->entityManager->persist($page);
+
+        $route = new AlphaRoute();
+        $route->setName($routeName);
+        $route->setRoute('/' . $routeName);
+        $route->setDefaultController('AlphaPage\Controller\Page');
+        $route->setDefaultAction('view');
+        $route->setType(AlphaRoute::TYPE_LITERAL);
+        $route->setParentRoute(NULL);
+        $route->setPage($page);
+        $this->entityManager->persist($route);
+
+        $this->entityManager->flush();
+    }
+
+    public function deletePage($id) {
+
+        $page = $this->entityManager->getRepository('AlphaPage\Entity\Page')->find($id);
+        if (!empty($page)) {
+            $route = $this->entityManager->getRepository('Alpha\Entity\AlphaRoute')->findOneBy(['page' => $page]);
+            if (!empty($route)) {
+                $this->entityManager->remove($route);
+                $this->entityManager->flush();
+            }
+            $this->entityManager->remove($page);
+            $this->entityManager->flush();
+        }
     }
 
 }
